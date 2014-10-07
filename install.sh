d782b2c6158dc2f35bbfbb72a86d0f1cd4d36210
wfkoch <wfkoch@gmail.com>
2014-10-06 22:08:56 -0400

    Adding install file

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Download virtualenv
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
+#Unpacked it
+tar xvfz virtualenv-1.9.tar.gz
+cd virtualenv-1.9
+#This creates the virtual environment named ve_pa0
+python virtualenv.py ve_pa0
+cd ve_pa0
+
+#activate ve and now pip is available to install dependencies
+source bin/activate
+#Install simpy
+pip install simpy
+
+#now run our code
+#python <script to run>.py
+#get out of ve
+deactivate
