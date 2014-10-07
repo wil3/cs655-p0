a36ad7ace99383e3b8a72b287f85c3329c1df875
wfkoch <wfkoch@gmail.com>
2014-10-07 17:01:24 -0400

    updating installer, name change for main.py

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@ curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.
 tar xvfz virtualenv-1.9.tar.gz
 cd virtualenv-1.9
 #This creates the virtual environment named ve_pa0
-python virtualenv.py ve_pa0
+python virtualenv.py --system-site-packages ve_pa0
 cd ve_pa0
 
 #activate ve and now pip is available to install dependencies
 source bin/activate
 #Install simpy
 pip install simpy
-
 #now run our code
 #python <script to run>.py
 #get out of ve
